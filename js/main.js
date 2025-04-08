8629b1235bf1aa7116839b8f173af275ada09518
Ahmed <ahmedabdelgleel445@gmail.com>
2025-04-08 16:47:23 +0200

    setup Project structure

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let CairoTower = document.getElementById("Cairo Tower");
+let AlAzharPark = document.getElementById("Al-Azhar Park");
+let WadiDeglaReserve = document.getElementById("Wadi Degla Reserve");
+let AlAzharMosque = document.getElementById("Al-Azhar Mosque");
+let EgyptianMuseum = document.getElementById("Egyptian Museum");
+let HangingChurch= document.getElementById("Hanging Church");
+let MuizzStreet = document.getElementById("Muizz Street");
+
+CairoTower.onclick=function(){
+    window.location.replace("Cairo Tower/main.html");   
+}
+AlAzharPark.onclick=function(){
+    window.location.replace("Al-Azhar Park/main.html");
+}
+WadiDeglaReserve.onclick=function(){
+    window.location.replace("Wadi Degla Reserve/main.html");   
+}
+EgyptianMuseum.onclick=function(){
+    window.location.replace("Egyptian Museum/main.html");   
+}
+HangingChurch.onclick=function(){
+    window.location.replace("Hanging Church/main.html");   
+}
+AlAzharMosque.onclick=function(){
+    window.location.replace("Al-Azhar Mosque/main.html");   
+}
+MuizzStreet.onclick=function(){
+    window.location.replace("Muizz Street/main.html");   
+}
